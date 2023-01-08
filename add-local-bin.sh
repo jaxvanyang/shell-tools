a51c924feda00cb48b4571dcab3d158b5c2d8bd0
Jax Young <jaxvanyang@gmail.com>
2023-01-08 11:57:07 +0800

    refactor(add-local-bin.sh): rename & use wrapper script method
    Rename add_my_bin.sh to add-local-bin and use wrapper script method
    istead of "ln -s" method. The standard way to add a binary to /bin
    is the wrapper script method. Because it won't break relative path
    dependencies, but "ln -s" method will.
    
    Also add more detailed help message.

---
+++ add-local-bin.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+set -e
+
+# set PREFIX if you want to modify installation location
+PREFIX="${PREFIX:=$HOME/.local/bin}"
+
+# TODO: use signal to trigger help_and_exit()
+help_and_exit() {
+		echo "NAME"
+		echo -e "\t$0 - add an executable to your local bin path, e.g., \$home/.local/bin"
+		echo
+		echo "SYNOPSIS"
+		echo -e "\t$0 <executable> [alias]"
+		echo
+		echo "DESCRIPTION"
+		echo -e "\tThis command will create a wrapper script in \$PREFIX, which is \$HOME/.local/bin by default."
+		echo -e "\tThe wrapper script is named as the same as the executable's basename. If the alias argument is"
+		echo -e "\tprovided, it will be named as the alias argument."
+		echo
+		echo "EXAMPLES"
+		echo -e "\t$0 hello"
+		echo -e "\t\tCreate a wrapper script for hello in current directory."
+		echo
+		echo -e "\t$0 /path/to/hello"
+		echo -e "\t\tCreate a wrapper script for hello using absolute path."
+		echo
+		echo -e "\t$0 hello foo"
+		echo -e "\t\tCreate a wrapper script for hello, but name it as foo."
+		echo
+		echo -e "\tPREFIX=/path/to/install $0 hello"
+		echo -e "\t\tCreate a wrapper script for hello, but in a custom directory."
+
+		exit 1
+}
+
+main() {
+	# only accept 1 or 2 command arguments
+	if [[ $# -lt 1 || $# -gt 2 ]]; then
+		help_and_exit
+	fi
+
+	name="$(basename "$1")"
+	executable="$1"
+
+	# Change executable to absolute path if it's relative path
+	if [[ ! $executable =~ ^/.*$ ]]; then
+		executable="$PWD/$executable"
+	fi
+
+	# Check if executable exists
+	if [ ! -f "$executable" ]; then
+		help_and_exit
+	fi
+
+	# Use alias if provided
+	if [ $# = 2 ]; then
+		name="$2"
+	fi
+
+	wrapper="$PREFIX/$name"
+
+	# Create the wrapper script
+	cat > "$wrapper" <<EOF
+#/usr/bin/env bash
+
+exec "$executable" "\$@"
+EOF
+	chmod +x "$wrapper"
+}
+
+main "$@"
