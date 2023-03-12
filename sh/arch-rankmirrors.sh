bbd70d388552f2dc81c19b57719a5b34c275e7bc
Jax Young <jaxvanyang@gmail.com>
2023-03-12 17:18:55 +0800

    feat(rankmirrors): add argument & usage comments

--- sh/arch-rankmirrors.sh
+++ sh/arch-rankmirrors.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+#
+# Select fastest mirrors of Arch Linux.
+#
+# Usage:
+# 	arch-rankmirrors [mirror_count]
+#
 # Modified from https://wiki.archlinux.org/title/Mirrors#List_by_speed
 
-curl -s "https://archlinux.org/mirrorlist/?country=CN&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 -
+arch_rankmirrors() {
+	curl -s "https://archlinux.org/mirrorlist/?country=CN&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n "${1}" -
+}
+
+main() {
+	if [ $# -gt 1 ]; then
+		echo "Only 0 or 1 argument is acceptable!" >&2
+		exit 1
+	fi
+
+	if [ $# -eq 0 ]; then
+		arch_rankmirrors 5
+	else
+		arch_rankmirrors "${1}"
+	fi
+}
+
+main "$@"
